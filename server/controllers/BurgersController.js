c81e6fba281057439ec2ffaced8ae0aaa27237ab
Timothy Kimble <timble417@gmail.com>
2021-07-26 14:20:46 -0600

    Made some good burgers

---
+++ server/controllers/BurgersController.js
@@ -0,0 +1,61 @@
+import { burgersService } from '../services/BurgersService'
+import BaseController from '../utils/BaseController'
+
+export class BurgersController extends BaseController {
+  constructor() {
+    super('api/burgers')
+    this.router
+      .get('', this.getAll)
+
+      .get('/:id', this.getById)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
+
+  getAll(req, res, next) {
+    try {
+      const burgers = burgersService.getAll()
+      res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  getById(req, res, next) {
+    try {
+      const burger = burgersService.getById(req.params.id)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  create(req, res, next) {
+    try {
+      const burger = burgersService.create(req.body)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      const burger = burgersService.edit(req.body)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  delete(req, res, next) {
+    try {
+      burgersService.delete(req.params.id)
+      res.send('Deleted')
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ server/db/fakeDb.js
@@ -0,0 +1,16 @@
+export const fakeDb = {
+  burgers: [{
+    id: 1,
+    name: 'Crispy Chicken',
+    contents: 'Cheese, Lettuce, Chicken, Tomato'
+  }, {
+    id: 2,
+    name: 'BigBoi Beef',
+    contents: 'Beef, Cheese, Lettuce, Tomato, Onion'
+  }, {
+    id: 3,
+    name: 'Lazy Lettuce',
+    contents: 'Lettuce, tomato, tofu'
+  }
+  ]
+}
